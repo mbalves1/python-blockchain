5acc601c23ad1a43b09e9910cbb042dd4bca7c74
Murilo <mbalves1@outlook.com>
2024-08-12 08:50:37 -0300

    [feat]: added Flask web app and /mine_block endpoint to blockchain implementation

--- blockchain.py
+++ blockchain.py
@@ -58,5 +58,25 @@ class Blockchain:
 # Part 2 - Mining our Blockchain
 
 # Creating a Web App
+app = Flask(__name__)
 
 # Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+  previous_block = blockchain.get_previous_block()
+  previous_proof = previous_block['proof']
+  proof = blockchain.proof_of_work(previous_proof)
+  previous_hash = blockchain.hash(previous_block)
+  block = blockchain.create_block(proof, previous_hash)
+  response = {
+    'message': 'Congratulations, you just mined a block!',
+    'index': block['index'],
+    'timestamp': block['timestam'],
+    'proof': block['proof'],
+    'previous_hash': block['previous_hash'],
+  }
+
+  return jsonify(response), 200
